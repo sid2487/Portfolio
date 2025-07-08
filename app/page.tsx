3bfeab767e969c83d0ec5b9915feaa145eab2c6b
sid2487 <sid454804@gmail.com>
2025-07-08 18:51:42 +0530

    fixed the second error

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Project from "@/components/Project";
 import Experience from "@/components/Experience";
 import Education from "@/components/Education";
 import Skills from "@/components/Skills";
-import Contact from "@/components/Contact";
 
 export default function HomePage() {
   return (
